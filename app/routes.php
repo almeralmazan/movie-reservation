c9ed35336c134bfff90def28c303dc435ca7a9e6
Almer Almazan <almeralmazan@gmail.com>
2014-06-26 12:46:51 +0800

    Add routes for admin

--- app/routes.php
+++ app/routes.php
@@ -1,21 +1,45 @@
 <?php
 
+// Public Pages
 Route::get('/', 'HomeController@index');
 
-// Member Page
+
+// Member Pages
+Route::group(['prefix' => 'member'], function()
+{
+    // GET
+    Route::post('login', 'MemberController@store');
+    Route::post('signup', 'MemberController@create');
+});
+
 Route::group(['before' => 'auth', 'prefix' => 'member'], function()
 {
     Route::get('/', 'MemberController@index');
     Route::get('logout', 'MemberController@destroy');
     Route::get('reserve/movie/{movieId}', 'MemberController@reserve');
+
+    // AJAX
+    Route::get('movie/{movieId}/{timeId}', 'MemberController@checkReserveSeats');
+    Route::post('save-reserved-seats', 'MemberController@saveReservedSeats');
 });
 
+
 // Admin Pages
+Route::group(['prefix' => 'admin'], function()
+{
+    // Public
+    Route::get('/', 'AdminController@index');
+    Route::post('login', 'AdminController@store');
+});
+
 Route::group(['before' => 'auth|admin', 'prefix' => 'admin'], function()
 {
     Route::get('logout', 'AdminController@destroy');
     Route::post('save-reserved-seats', 'AdminController@saveReservedSeats');
     Route::get('movie/delete/{movieId}', 'AdminController@deleteMovie');
+
+    // Admin AJAX
+    Route::get('movie/{movieId}', 'AdminController@showMovie');
 });
 
 Route::group(['before' => 'auth|admin', 'prefix' => 'admin/dashboard'], function()
