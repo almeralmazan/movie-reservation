e6635c1537d9dd046c2803f91b15e18357711701
Almer Almazan <almeralmazan@gmail.com>
2014-06-26 11:20:25 +0800

    Create layout page for home page

---
+++ app/views/layouts/main.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>{{ $title }}</title>
+    {{ HTML::style('css/foundation.min.css') }}
+    {{ HTML::style('css/foundation-icons.css') }}
+    {{ HTML::style('css/style.css') }}
+    {{ HTML::script('js/vendor/modernizr.js') }}
+</head>
+<body>
+
+<div class="content row">
+    @yield('content')
+</div>
+
+{{ HTML::script('js/vendor/jquery.min.js') }}
+</body>
+</html>
