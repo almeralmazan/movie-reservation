966296b091b188353f8a48d7baeae74a9d25d77c
Almer Almazan <almeralmazan@gmail.com>
2014-06-27 18:08:53 +0800

    Add getAllTimes() method is Movie model

--- app/models/Movie.php
+++ app/models/Movie.php
@@ -2,8 +2,16 @@
 
 class Movie extends Eloquent {
 
-    public function times()
+    public static function getAllTimes($id)
     {
-        $this->hasMany('Time');
+        $result = DB::table('movies')
+                    ->join('cinemas', 'movies.cinema_id', '=', 'cinemas.id')
+                    ->join('cinema_time', 'cinemas.id', '=', 'cinema_time.cinema_id')
+                    ->join('times', 'cinema_time.time_id', '=', 'times.id')
+                    ->where('movies.id', $id)
+                    ->select('times.start_time')
+                    ->get();
+
+        return $result;
     }
 }
