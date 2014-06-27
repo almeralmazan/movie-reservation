58e6a52d5709f16c23048d669fab0973e3023ea6
Almer Almazan <almeralmazan@gmail.com>
2014-06-27 15:14:01 +0800

    Remove to field and replace from --> start_time column in times table

--- app/database/migrations/2014_06_27_015530_create_times_table.php
+++ app/database/migrations/2014_06_27_015530_create_times_table.php
@@ -15,8 +15,7 @@ class CreateTimesTable extends Migration {
         Schema::create('times', function($table)
         {
             $table->increments('id');
-            $table->time('from');
-            $table->time('to');
+            $table->time('start_time');
         });
 	}
 
