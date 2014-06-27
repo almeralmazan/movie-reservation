2640a9464ba1f5560a41fc025e2b68bb6150cca4
Almer Almazan <almeralmazan@gmail.com>
2014-06-27 18:08:24 +0800

    Add list of times per movie

--- app/views/home/show.blade.php
+++ app/views/home/show.blade.php
@@ -7,4 +7,9 @@
     <p>{{ $movie->description }}</p>
     <p>Cinema: {{ $movie->cinema_id }}</p>
     <p>Showing Time: </p>
+    <ul>
+        @foreach ($movieTimes as $movie)
+        <li>{{ $movie->start_time }}</li>
+        @endforeach
+    </ul>
 @stop
