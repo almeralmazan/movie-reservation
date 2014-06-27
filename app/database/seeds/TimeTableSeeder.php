c25bf780e3fa0d393a5165a12f52790a3d611656
Almer Almazan <almeralmazan@gmail.com>
2014-06-27 16:16:14 +0800

    Create time records

--- app/database/seeds/TimeTableSeeder.php
+++ app/database/seeds/TimeTableSeeder.php
@@ -1,18 +1,35 @@
 <?php
 
+use Carbon\Carbon;
+
 class TimeTableSeeder extends Seeder {
 
     public function run()
     {
         DB::table('times')->delete();
+        $dt = new Carbon;
 
-        Time::create([
-
-        ]);
-
-        Time::create([
-
-        ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(10, 0, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(10, 30, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(11, 00, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(11, 30, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(12, 0, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(12, 30, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(13, 0, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(13, 30, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(14, 0, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(14, 30, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(15, 0, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(15, 30, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(16, 0, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(16, 30, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(17, 0, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(17, 30, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(18, 0, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(18, 30, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(19, 0, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(19, 30, 0) ]);
+        Time::create([ 'start_time'  =>  $dt->setTime(20, 0, 0) ]);
 
     }
 }
