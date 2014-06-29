edde0dcd719bb21927b3b9fc71cc5f0913aea8b6
Almer Almazan <almeralmazan@gmail.com>
2014-06-29 09:58:30 +0800

    Add style rules

--- app/views/member/index.blade.php
+++ app/views/member/index.blade.php
@@ -1,7 +1,36 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>Member Page</h1>
+<div class="row">
+    <div class="large-12 columns movie-showing-title">
+        <h1>Movies showing</h1>
+    </div>
+</div>
+<div class="row">
+    <div class="large-8 medium-11 panel movie-showing-container radius">
+        @foreach ($movies as $movie)
+        <div class="row">
+            <div class="large-4 medium-5 columns column-left">
+                {{ HTML::image('img/movies/' . $movie->image) }}
+            </div>
+            <div class="large-8 medium-7 columns column-right">
+                <h3>{{ $movie->title }}</h3>
+                <hr/>
+                <dl>
+                    <dt>Sypnosis</dt>
+                    <dd>{{ $movie->description }}</dd>
+                </dl>
+                <!-- TODO -->
+                {{ HTML::link('#', 'Reserve seat', ['class' => 'tiny radius button']) }}
+            </div>
+        </div>
+        <hr class="divider">
+        @endforeach
 
-    {{ HTML::link('member/logout', 'Logout', ['class' => 'button']) }}
+    </div>
+</div>
+<div class="row">
+    <hr>
+</div>
 @stop
+
