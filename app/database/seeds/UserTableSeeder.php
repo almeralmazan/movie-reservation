33eb8c764047527c9480c690ff4b928fb604b992
Almer Almazan <almeralmazan@gmail.com>
2014-07-18 20:59:14 +0800

    Add walkin and member users

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -21,7 +21,43 @@ class UserTableSeeder extends Seeder {
             'email'         =>  'ayi@gmail.com',
             'password'      =>  Hash::make('ayi'),
             'mobile_number' =>  '+639353626640',
-            'admin'         =>  0
+            'admin'         =>  2   // member
+        ]);
+
+        User::create([
+            'first_name'    =>  'xian',
+            'last_name'     =>  'yancy',
+            'email'         =>  'xian@gmail.com',
+            'password'      =>  Hash::make('xian'),
+            'mobile_number' =>  '+639336013797',
+            'admin'         =>  2   // member
+        ]);
+
+        User::create([
+            'first_name'    =>  'arnold',
+            'last_name'     =>  'piala',
+            'email'         =>  'arnold@gmail.com',
+            'password'      =>  Hash::make('arnold'),
+            'mobile_number' =>  '+639494149766',
+            'admin'         =>  2   // member
+        ]);
+
+        User::create([
+            'first_name'    =>  'juan',
+            'last_name'     =>  'dela cruz',
+            'email'         =>  'juandelacruz@gmail.com',
+            'password'      =>  Hash::make('juan'),
+            'mobile_number' =>  '+639159115122',
+            'admin'         =>  0   // walkin
+        ]);
+
+        User::create([
+            'first_name'    =>  'mari',
+            'last_name'     =>  'mar',
+            'email'         =>  'marimar@gmail.com',
+            'password'      =>  Hash::make('marimar'),
+            'mobile_number' =>  '+639159222345',
+            'admin'         =>  0   // walkin
         ]);
     }
 }
