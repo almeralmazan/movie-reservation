d8ab6910a759de0d60564763f84f34f2dc1b50a8
Almer Almazan <almeralmazan@gmail.com>
2014-07-20 12:18:53 +0800

    Fix type created_ad to created_at in admin/member.blade.php

--- app/views/admin/member.blade.php
+++ app/views/admin/member.blade.php
@@ -29,7 +29,7 @@
                         <td>{{ $user->first_name .' '. $user->last_name }}</td>
                         <td>{{ $user->email }}</td>
                         <td>{{ $user->mobile_number }}</td>
-                        <th>{{ date('F j, Y', strtotime($user->created_ad)) }}</th>
+                        <th>{{ date('F j, Y', strtotime($user->created_at)) }}</th>
                     </tr>
                 @endforeach
                 </tbody>
