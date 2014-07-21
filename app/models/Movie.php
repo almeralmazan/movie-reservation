9fe448ac330a0e9b24e72f2bad2bda7bfce5b319
Almer Almazan <almeralmazan@gmail.com>
2014-07-21 18:06:44 +0800

    Add times.id in select

--- app/models/Movie.php
+++ app/models/Movie.php
@@ -11,7 +11,7 @@ class Movie extends Eloquent {
                     ->join('cinema_time', 'cinemas.id', '=', 'cinema_time.cinema_id')
                     ->join('times', 'cinema_time.time_id', '=', 'times.id')
                     ->where('movies.id', $id)
-                    ->select('times.start_time')
+                    ->select('times.id', 'times.start_time')
                     ->get();
 
         return $result;
