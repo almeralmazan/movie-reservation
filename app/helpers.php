34ee8911f559564a57c5625ef9782706adfa9f0f
Almer Almazan <almeralmazan@gmail.com>
2014-08-08 19:02:30 +0800

    Add show_times() helper function

---
+++ app/helpers.php
@@ -0,0 +1,11 @@
+<?php
+
+function show_times($movieId)
+{
+    return DB::table('times')
+            ->select('times.start_time')
+            ->join('cinema_time', 'cinema_time.time_id', '=', 'times.id')
+            ->join('movies', 'movies.cinema_id', '=', 'cinema_time.cinema_id')
+            ->where('movies.id', $movieId)
+            ->get();
+}
