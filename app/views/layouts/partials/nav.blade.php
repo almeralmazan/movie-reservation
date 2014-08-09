05679bf88b4448580720605285e575b62a17090f
Almer Almazan <almeralmazan@gmail.com>
2014-08-09 13:17:33 +0800

    Remove the word Action

--- app/views/layouts/partials/nav.blade.php
+++ app/views/layouts/partials/nav.blade.php
@@ -40,7 +40,7 @@
                 </li>
 
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Action <span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
                         <li>
                             <a href="{{ URL::to('member/profile') }}"><span class="glyphicon glyphicon-user"></span> &nbsp;Profile</a>
