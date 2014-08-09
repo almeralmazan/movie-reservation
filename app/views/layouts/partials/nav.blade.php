6dc9fb57cc83f4caaf7f5729bbc7e04cd4595b61
Almer Almazan <almeralmazan@gmail.com>
2014-08-09 20:24:21 +0800

    Fix logout icon for admin and member

--- app/views/layouts/partials/nav.blade.php
+++ app/views/layouts/partials/nav.blade.php
@@ -39,20 +39,26 @@
                     <p class="navbar-text">Welcome - {{ Auth::user()->first_name }}</p>
                 </li>
 
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li>
-                            <a href="{{ URL::to('member/profile') }}"><span class="glyphicon glyphicon-user"></span> &nbsp;Profile</a>
-                        </li>
-                        <li>
-                            <a href="{{ URL::to('member/transaction') }}"><span class="glyphicon glyphicon-tasks"></span> &nbsp;Transactions</a>
-                        </li>
-                        <li>
-                            <a href="{{ URL::to('member/logout') }}"><span class="glyphicon glyphicon-off"></span> &nbsp;Logout</a>
-                        </li>
-                    </ul>
-                </li>
+                @if (Auth::user()->first_name == 'admin')
+                    <li>
+                        <a href="{{ URL::to('member/logout') }}"><span class="glyphicon glyphicon-off"></span> &nbsp;Logout</a>
+                    </li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="{{ URL::to('member/profile') }}"><span class="glyphicon glyphicon-user"></span> &nbsp;Profile</a>
+                            </li>
+                            <li>
+                                <a href="{{ URL::to('member/transaction') }}"><span class="glyphicon glyphicon-tasks"></span> &nbsp;Transactions</a>
+                            </li>
+                            <li>
+                                <a href="{{ URL::to('member/logout') }}"><span class="glyphicon glyphicon-off"></span> &nbsp;Logout</a>
+                            </li>
+                        </ul>
+                    </li>
+                @endif
 
             </ul>
         @endif
