adf9cb89cc684540785e711dc5d28f3c645b4ccc
Almer Almazan <almeralmazan@gmail.com>
2014-08-09 14:06:32 +0800

    Add dynamic values for edit account

--- app/views/member/partials/edit-account.blade.php
+++ app/views/member/partials/edit-account.blade.php
@@ -8,8 +8,14 @@
             </div>
             <div class="modal-body">
                 <form action="" class="form-horizontal">
-                    <label for="name">Name</label><input type="text" id="name" class="form-control" value="Jenary Madia">
-                    <label for="contact-no">Contact #</label><input type="text" id="contact-no" class="form-control" value="09353626640">
+                    <label for="first-name">First Name</label>
+                    <input type="text" id="first-name" class="form-control" value="{{ $member->first_name }}">
+
+                    <label for="last-name">Last Name</label>
+                    <input type="text" id="last-name" class="form-control" value="{{ $member->last_name }}">
+
+                    <label for="contact-no">Contact #</label>
+                    <input type="text" id="contact-no" class="form-control" value="{{ $member->mobile_number }}">
                 </form>
             </div>
             <div class="modal-footer">
