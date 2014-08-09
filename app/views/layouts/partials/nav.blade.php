d9d0c01ba0070a34c98da3103da625541b9303a7
Almer Almazan <almeralmazan@gmail.com>
2014-08-09 13:11:13 +0800

    Add actions for member profile and transactions

--- app/views/layouts/partials/nav.blade.php
+++ app/views/layouts/partials/nav.blade.php
@@ -38,9 +38,22 @@
                 <li>
                     <p class="navbar-text">Welcome - {{ Auth::user()->first_name }}</p>
                 </li>
-                <li>
-                    <a href="{{ URL::to('member/logout') }}"><span class="glyphicon glyphicon-off"></span> Logout</a>
+
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Action <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li>
+                            <a href="{{ URL::to('member/profile') }}"><span class="glyphicon glyphicon-user"></span> &nbsp;Profile</a>
+                        </li>
+                        <li>
+                            <a href="{{ URL::to('member/transaction') }}"><span class="glyphicon glyphicon-tasks"></span> &nbsp;Transactions</a>
+                        </li>
+                        <li>
+                            <a href="{{ URL::to('member/logout') }}"><span class="glyphicon glyphicon-off"></span> &nbsp;Logout</a>
+                        </li>
+                    </ul>
                 </li>
+
             </ul>
         @endif
         </div><!-- /.navbar-collapse -->
