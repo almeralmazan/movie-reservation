ee942b6bb8f3d92f9cd49ad9583bea6967d48774
Almer Almazan <almeralmazan@gmail.com>
2014-08-09 11:57:20 +0800

    Set color to white in footer

--- app/views/layouts/partials/footer.blade.php
+++ app/views/layouts/partials/footer.blade.php
@@ -9,7 +9,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <span>&copy; Online movie reservation 2014</span>
+                <span style="color: #ffffff">&copy; Online movie reservation 2014</span>
             </div>
             <div class="col-md-6">
                 <!-- Icons here -->
