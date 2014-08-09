20e0ccd01221571593a1c17b0b387c6f2cfa445a
Almer Almazan <almeralmazan@gmail.com>
2014-08-09 15:25:27 +0800

    Populate data of specific number in edit account form

--- app/views/member/partials/edit-account.blade.php
+++ app/views/member/partials/edit-account.blade.php
@@ -6,22 +6,22 @@
                 <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
                 <h4 class="modal-title" id="myModalLabel">Edit Account</h4>
             </div>
-            <div class="modal-body">
-                <form action="" class="form-horizontal">
-                    <label for="first-name">First Name</label>
-                    <input type="text" id="first-name" class="form-control" value="{{ $member->first_name }}">
+            {{ Form::open(['url' => 'member/update-account', 'class' => 'form-horizontal']) }}
+                <div class="modal-body">
+                        <label for="first-name">First Name</label>
+                        <input type="text" id="first-name" name="first_name" class="form-control" value="{{ $member->first_name }}">
 
-                    <label for="last-name">Last Name</label>
-                    <input type="text" id="last-name" class="form-control" value="{{ $member->last_name }}">
+                        <label for="last-name">Last Name</label>
+                        <input type="text" id="last-name" name="last_name" class="form-control" value="{{ $member->last_name }}">
 
-                    <label for="contact-no">Contact #</label>
-                    <input type="text" id="contact-no" class="form-control" value="{{ $member->mobile_number }}">
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
+                        <label for="contact-no">Contact #</label>
+                        <input type="text" id="contact-no" name="mobile_number" class="form-control" value="{{ $member->mobile_number }}">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+            {{ Form::close() }}
         </div>
     </div>
 </div>
